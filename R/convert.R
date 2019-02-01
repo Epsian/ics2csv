aa8ccd3efac7508ce552e31191bee8200c7f2501
Epsian <jared.n.joseph@gmail.com>
2019-02-01 10:12:06 -0800

    package bugfixes

--- R/convert.R
+++ R/convert.R
@@ -8,23 +8,20 @@
 #                                                     #
 #######################################################
 
-ics2csv = function(ics){
-  #### Setup ####
-  # Required Packages
-  library(stringr)
-  library(lubridate)
-  library(plyr)
+#' @export
+
+ics2csv = function(ics_file_path){
 
   #### Convert ####
   # Read in ical
-  cal = readLines(ics)
+  cal = readLines(ics_file_path)
 
   # Trim out metadata
   meta = cal[1:match("BEGIN:VEVENT", cal)-1]
   cal = cal[match("BEGIN:VEVENT", cal):length(cal)]
 
   # Convert each event to list element
-  chunks = data.frame(starts = str_which(cal, "BEGIN:VEVENT"), ends = str_which(cal, "END:VEVENT"))
+  chunks = data.frame(starts = stringr::str_which(cal, "BEGIN:VEVENT"), ends = stringr::str_which(cal, "END:VEVENT"))
   entries = apply(chunks, 1, FUN = function(x) (x[1]+1):(x[2]-1))
 
   entries = lapply(entries, FUN = function(x){
@@ -34,7 +31,7 @@ ics2csv = function(ics){
     .starters = c("DTSTART:", "DTEND:", "DTSTAMP:", "UID:", "CREATED:", "DESCRIPTION:", "LAST-MODIFIED:", "LOCATION:", "SEQUENCE:", "STATUS:", "SUMMARY:", "TRANSP:", "RRULE:", "EXDATE;")
 
     # Find those that are fragments, past with prior entry
-    info_fragment = !str_detect(event, paste(.starters, collapse = "|"))
+    info_fragment = !stringr::str_detect(event, paste(.starters, collapse = "|"))
     event[info_fragment] = trimws(event)[info_fragment]
 
     while(any(info_fragment)){
@@ -44,7 +41,7 @@ ics2csv = function(ics){
     }
 
     # Turn each event into data frame
-    event = data.frame(t(str_split(event, ":", n = 2, simplify = TRUE)), stringsAsFactors = FALSE)
+    event = data.frame(t(stringr::str_split(event, ":", n = 2, simplify = TRUE)), stringsAsFactors = FALSE)
     colnames(event) = event[1,]
     event = event[-1,]
   })
@@ -53,12 +50,13 @@ ics2csv = function(ics){
   entries = plyr::rbind.fill(entries)
 
   # Convert dates to lubridate
-  entries$DTSTART = as_datetime(entries$DTSTART)
-  entries$DTEND = as_datetime(entries$DTEND)
-  entries$DTSTAMP = as_datetime(entries$DTSTAMP)
-  entries$CREATED = as_datetime(entries$CREATED)
-  entries$`LAST-MODIFIED` = as_datetime(entries$`LAST-MODIFIED`)
+  entries$DTSTART = lubridate::as_datetime(entries$DTSTART)
+  entries$DTEND = lubridate::as_datetime(entries$DTEND)
+  entries$DTSTAMP = lubridate::as_datetime(entries$DTSTAMP)
+  entries$CREATED = lubridate::as_datetime(entries$CREATED)
+  entries$`LAST-MODIFIED` = lubridate::as_datetime(entries$`LAST-MODIFIED`)
 
+  return(entries)
 }
 
 
